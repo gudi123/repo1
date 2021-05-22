b4a263468e9abf2b7796bc159b5cec1a2dd09967
gudi123 <83576922+gudi123@users.noreply.github.com>
2021-05-23 00:14:56 +0530

    Update builImage.sh

--- builImage.sh
+++ builImage.sh
@@ -1,6 +1,6 @@
 version=$BUILD_ID
 docker build -t docker-repo111:$version .
-aws ecr get-login-password --region us-east-1 docker login --username AWS --password-stdin 503983346536.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 503983346536.dkr.ecr.us-east-1.amazonaws.com
 docker tag docker-repo111:$version 503983346536.dkr.ecr.us-east-1.amazonaws.com/docker-repo111:$version
 docker push 503983346536.dkr.ecr.us-east-1.amazonaws.com/docker-repo111:$version
 echo "images sucessfully executed"
