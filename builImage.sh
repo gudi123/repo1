b870126f0162cee37f4bd6e6da8ecf96b11dca44
root <root@ip-172-31-23-109.ec2.internal>
2021-05-22 17:38:36 +0000

    updated

--- builImage.sh
+++ builImage.sh
@@ -1,5 +1,5 @@
 name=$2
-version=$1
+version=$BUILD_ID
 docker build -t docker-repo111:$version .
 aws ecr get-login-password --region us-east-1 --profile rani| docker login --username AWS --password-stdin 503983346536.dkr.ecr.us-east-1.amazonaws.com
 docker tag docker-repo111:$version 503983346536.dkr.ecr.us-east-1.amazonaws.com/docker-repo111:$version
