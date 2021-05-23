9a753a25e2343f69713b7501cd8d84fdac1b8b6f
gudi123 <83576922+gudi123@users.noreply.github.com>
2021-05-23 20:11:35 +0530

    Update builImage.sh

--- builImage.sh
+++ builImage.sh
@@ -1,4 +1,5 @@
 version=$BUILD_ID
+sudo bash
 docker build -t docker-repo123 .
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 503983346536.dkr.ecr.us-east-1.amazonaws.com
 docker tag docker-repo123:latest 503983346536.dkr.ecr.us-east-1.amazonaws.com/docker-repo123:latest
