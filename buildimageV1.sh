cf4f4d6ed83d2ccc49eb19226f9bc8317d1f0f05
gudi123 <83576922+gudi123@users.noreply.github.com>
2021-05-25 15:50:46 +0530

    Update buildimageV1.sh

--- buildimageV1.sh
+++ buildimageV1.sh
@@ -1,3 +1,4 @@
+version=$BUILD_ID
 docker build -t repo-new1 .
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 503983346536.dkr.ecr.us-east-1.amazonaws.com
 docker tag repo-new1:latest 503983346536.dkr.ecr.us-east-1.amazonaws.com/repo-new1:latest
