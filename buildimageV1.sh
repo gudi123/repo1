efd9eb8e03db6ce4de90469cb0791a15e9c841e8
gudi123 <83576922+gudi123@users.noreply.github.com>
2021-05-25 15:48:35 +0530

    Update buildimageV1.sh

--- buildimageV1.sh
+++ buildimageV1.sh
@@ -1,6 +1,5 @@
-version=$BUILD_ID
-docker build -t repo-new1$version .
+docker build -t repo-new1 .
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 503983346536.dkr.ecr.us-east-1.amazonaws.com
-docker tag repo-new1:$version 503983346536.dkr.ecr.us-east-1.amazonaws.com/repo-new1:$version
-docker push 503983346536.dkr.ecr.us-east-1.amazonaws.com/repo-new1:$version
+docker tag repo-new1:latest 503983346536.dkr.ecr.us-east-1.amazonaws.com/repo-new1:latest
+docker push 503983346536.dkr.ecr.us-east-1.amazonaws.com/repo-new1:latest
 echo "Image Successfully pused to ecr"
