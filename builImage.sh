b499e6c61f5b2c5fa0b6ca51b98d5e1080810fd9
gudi123 <83576922+gudi123@users.noreply.github.com>
2021-05-25 10:43:19 +0530

    Delete builImage.sh

--- builImage.sh
+++
@@ -1,7 +0,0 @@
-version=$BUILD_ID
-sudo bash
-docker build -t docker-repo123$version .
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 503983346536.dkr.ecr.us-east-1.amazonaws.com
-docker tag docker-repo123:$version 503983346536.dkr.ecr.us-east-1.amazonaws.com/docker-repo123:$version
-docker push 503983346536.dkr.ecr.us-east-1.amazonaws.com/docker-repo123:$version
-echo "images sucessfully executed"
